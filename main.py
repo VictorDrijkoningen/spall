2d28e8f16a248401bee81082b515df05c3420e35
Victor Drijkoningen <>
2024-04-17 19:30:10 +0200

    init webserver

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # pylint: skip-file
-
 import machine
 import ssd1306
 import time
 import urequests as requests
+import usocket as socket
 
 
 with open(".env") as f:
@@ -15,6 +15,7 @@ def update(force_update = False):
         spall_version = file.read()
         githubversion = requests.get(env[2]).text
         if spall_version != githubversion or force_update:
+            print(fileversion.split("-")[0]+" / "+ str(githubversion).split("-")[0])
             with open('VERSION', 'w') as f:
                   f.write(githubversion)
             with open('main.py', 'w') as f:
@@ -22,19 +23,59 @@ def update(force_update = False):
             with open('boot.py', 'w') as f:
                   f.write(requests.get(env[4]).text)
             print('done updating')
-    return spall_version, githubversion
+    return spall_version
 
 
-
-fileversion,githubversion = update()
+fileversion = update()
 
 
 i2c = machine.SoftI2C(sda=machine.Pin(22), scl=machine.Pin(21))
 display = ssd1306.SSD1306_I2C(128,64, i2c)
 
-display.text(fileversion.split("-")[0]+" / "+ str(githubversion).split("-")[0], 0,0,1)
-display.show()
-time.sleep(5)
-display.fill(0)
+display.text(fileversion.split("-")[0], 0,0,1)
 display.show()
 
+def servo():
+    right_motor = machine.PWM(machine.Pin(13))
+    right_motor.freq(50)
+    right_motor.duty(300)
+    time.sleep(3)
+    right_motor.deinit()
+
+servo()
+
+
+def web_page():
+  html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none; 
+  border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server</h1> 
+  <p>GPIO state: <strong>5555</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
+  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p></body></html>"""
+  return html
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 80))
+s.listen(5)
+exit()
+while True:
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  request = conn.recv(1024)
+  request = str(request)
+  print('Content = %s' % request)
+  led_on = request.find('/?led=on')
+  led_off = request.find('/?led=off')
+  if led_on == 6:
+    print('LED ON')
+    led.value(1)
+  if led_off == 6:
+    print('LED OFF')
+    led.value(0)
+  response = web_page()
+  conn.send('HTTP/1.1 200 OK\n')
+  conn.send('Content-Type: text/html\n')
+  conn.send('Connection: close\n\n')
+  conn.sendall(response)
+  conn.close()
