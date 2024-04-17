a6082e9a8cb2f50b7e137c3253803fc026397948
Victor Drijkoningen <>
2024-04-17 17:27:18 +0200

    check file version

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
 import machine
 import ssd1306
 import time
+import urequests as requests
 
 
-def http_get(url):
-    import socket
-    _, _, host, path = url.split('/', 3)
-    addr = socket.getaddrinfo(host, 80)[0][-1]
-    s = socket.socket()
-    s.connect(addr)
-    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
-    while True:
-        data = s.recv(100)
-        if data:
-            print(str(data, 'utf8'), end='')
-        else:
-            break
-    s.close()
+with open(".env") as f:
+        env = f.read().split(",")
+
 
 def update():
     with open("VERSION") as file:
         spall_version = file.read()
-    print(spall_version, "...")
+        githubversion = requests.get(env[2]).text
+        if spall_version != githubversion:
+             with open('VERSION', 'w') as f:
+                  f.write(githubversion)
+                  
+    return spall_version, githubversion
 
-    return spall_version
 
-fileversion = update()
 
+fileversion,githubversion = update()
 
 
 i2c = machine.SoftI2C(sda=machine.Pin(22), scl=machine.Pin(21))
 display = ssd1306.SSD1306_I2C(128,64, i2c)
 
-display.text(fileversion.split("-")[0], 0,0,1)
+display.text(fileversion.split("-")[0]+" / "+ str(githubversion), 0,0,1)
 display.show()
-time.sleep(2)
+time.sleep(5)
 display.fill(0)
 display.show()
 
