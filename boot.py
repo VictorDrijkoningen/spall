905a1309d0f66e410019bc5bc08cb041f14ef73b
Victor Drijkoningen <>
2024-05-26 16:40:40 +0200

    wrap network code in try, and except with backup

--- boot.py
+++ boot.py
@@ -8,6 +8,7 @@ import network
 import webrepl
 import gc
 import esp
+import time
 
 esp.osdebug(0)
 
@@ -18,18 +19,25 @@ webrepl.start()
 mode = 'LAN'
 #mode = 'AP'
 
-if mode == 'AP':
+try:
+    if mode == 'AP':
+        ap = network.WLAN(network.AP_IF)
+        ap.config(essid='SPALL')
+        ap.active(True)
+    else:
+        with open(".env") as env_file:
+            env = env_file.read().split(",")
+        lan = network.WLAN(network.STA_IF)
+        lan.active(True)
+        lan.connect(env[0], env[1])
+        start = time.time()
+        while not lan.isconnected():
+            time.sleep(0.1)
+            if (time.time() - start) > 10:
+                raise TimeoutError
+except:
     ap = network.WLAN(network.AP_IF)
-    ap.config(essid='SPALL')
+    ap.config(essid='ERROR')
     ap.active(True)
-else:
-    with open(".env") as env_file:
-        env = env_file.read().split(",")
-    lan = network.WLAN(network.STA_IF)
-    lan.active(True)
-    lan.connect(env[0], env[1])
-    while not lan.isconnected():
-        pass
-
 
 gc.collect()
